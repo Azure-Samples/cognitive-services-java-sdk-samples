c4ef2dc4aec4e4ff6a5e260ab2106b7c0d99f808
Winona Azure <38537084+wiazur@users.noreply.github.com>
2019-08-30 19:21:31 -0700

    Updated key
    Changing all authentication variables to environment variables with standard naming.
    <SERVICE>_SUBSCRIPTION_KEY

--- Search/BingImageSearch/Quickstart/src/main/java/com/microsoft/azure/cognitiveservices/search/imagesearch/samples/BingImageSearchSample.java
+++ Search/BingImageSearch/Quickstart/src/main/java/com/microsoft/azure/cognitiveservices/search/imagesearch/samples/BingImageSearchSample.java
@@ -64,13 +64,13 @@ public class BingImageSearchSample {
         try {
             // Authenticate
 
-            // If you are going to set the AZURE_BING_SAMPLES_API_KEY environment variable, make sure you set it for your OS, then reopen your command prompt or IDE.
+            // If you are going to set the BING_SEARCH_V7_SUBSCRIPTION_KEY environment variable, make sure you set it for your OS, then reopen your command prompt or IDE.
             // If not, you may get an API key not found exception.
-            // IMPORTANT: if you have not set the `AZURE_BING_SAMPLES_API_KEY` environment variable to your cognitive services API key:
+            // IMPORTANT: if you have not set the BING_SEARCH_V7_SUBSCRIPTION_KEY environment variable to your cognitive services API key:
             // 1. comment out the below line
-            final String subscriptionKey = System.getenv("AZURE_BING_SAMPLES_API_KEY");
+            final String subscriptionKey = System.getenv("BING_SEARCH_V7_SUBSCRIPTION_KEY");
             // 2. paste your cognitive services API key below, and uncomment the line
-            //final String subscriptionKey = "enter your key here";
+            //final String subscriptionKey = System.getenv("BING_SEARCH_V7_SUBSCRIPTION_KEY");
             
             BingImageSearchAPI client = BingImageSearchManager.authenticate(subscriptionKey);
             String searchTerm = "canadian rockies";
