dc1d26d71db5a825861a60df214224af2431c42c
Winona Azure <38537084+wiazur@users.noreply.github.com>
2019-08-30 20:36:05 -0700

    Updated key
    Changing all authentication variables to environment variables with standard naming.

--- Search/BingWebSearch/src/main/java/com/microsoft/azure/cognitiveservices/search/websearch/samples/BingWebSearchSample.java
+++ Search/BingWebSearch/src/main/java/com/microsoft/azure/cognitiveservices/search/websearch/samples/BingWebSearchSample.java
@@ -121,14 +121,10 @@ public class BingWebSearchSample {
         try {
             //=============================================================
             // Authenticate
-
-            // If you are going to set the AZURE_BING_SAMPLES_API_KEY environment variable, make sure you set it for your OS, then reopen your command prompt or IDE.
-            // If not, you may get an API key not found exception.
+            // Set the BING_SEARCH_V7_SUBSCRIPTION_KEY environment variable with your subscription key,
+            // then reopen your command prompt or IDE. If not, you may get an API key not found exception.
             // IMPORTANT: if you have not set the `AZURE_BING_SAMPLES_API_KEY` environment variable to your cognitive services API key:
-            // 1. comment out the below line
-            final String subscriptionKey = System.getenv("AZURE_BING_SAMPLES_API_KEY");
-            // 2. paste your cognitive services API key below, and uncomment the line
-            //final String subscriptionKey = "enter your key here";
+            final String subscriptionKey = System.getenv("BING_SEARCH_V7_SUBSCRIPTION_KEY");
 
             BingWebSearchAPI client = BingWebSearchManager.authenticate(subscriptionKey);
 
