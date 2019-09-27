3df04c1e9ce08c9b9e1f24aab2d9a7002f4804a3
Aaron Hill <aahi@microsoft.com>
2019-09-27 11:14:24 -0700

    adding gradle functionality

---
+++ Search/BingVisualSearch/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    java
+    application
+}
+application {
+    mainClassName = "main.java.BingVisualSearchSample"
+}
+repositories {
+    mavenCentral()
+}
+dependencies {
+    compile("org.slf4j:slf4j-simple:1.7.25")
+    compile("com.microsoft.azure.cognitiveservices:azure-cognitiveservices-visualsearch:1.0.2-beta")
+    compile("com.google.code.gson:gson:2.8.5")
+}
